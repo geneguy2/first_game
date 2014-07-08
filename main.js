555b220ccec95fe567626deb323cdd8706be42ce
Gene Guy <gene@geneguy.com>
2014-07-08 17:59:57 -0400

    Updated the index.html and main.js files

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ var mainState = {
 preload: function() {
 // This function will be executed at the beginning
 // That's where we load the game's assets
+
 },
 create: function() {
 // This function is called after the preload function
 // Here we set up the game, display sprites, etc.
+// Make the stage color blue
+game.stage.backgroundColor = '#3498db';
+
+// Create the physics engine
+game.physics.startSystem(Phaser.Physics.ARCADE);
 },
 update: function() {
 // This function is called 60 times per second
@@ -15,3 +21,10 @@ update: function() {
 },
 // And here we will later add some of our own functions
 };
+
+// We initialise Phaser
+// Create a 500px by 340px game in the 'gameDiv' element of the index.html
+var game = new Phaser.Game(500, 340, Phaser.AUTO, 'gameDiv');
+// Add the 'mainState' to Phaser, and call it 'main'
+game.state.add('main', mainState);
+game.state.start('main');
