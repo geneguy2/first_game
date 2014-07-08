71eedbd6ea5e1c32aae4fc898695dfef65fe70b1
Gene Guy <gene@geneguy.com>
2014-07-08 17:43:33 -0400

    First Phaser Game
    Initial Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// We create our only state, called 'mainState'
+var mainState = {
+// We define the 3 default Phaser functions
+preload: function() {
+// This function will be executed at the beginning
+// That's where we load the game's assets
+},
+create: function() {
+// This function is called after the preload function
+// Here we set up the game, display sprites, etc.
+},
+update: function() {
+// This function is called 60 times per second
+// It contains the game's logic
+},
+// And here we will later add some of our own functions
+};
