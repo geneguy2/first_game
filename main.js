214fe11392dac56215c775f56bc01b3fcd3f8608
Gene Guy <gene@geneguy.com>
2014-07-08 22:33:46 -0400

    Updated main.js.

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ preload: function() {
 // That's where we load the game's assets
 
 game.load.image('player', 'assets/player.png');
+game.load.image('wallV', 'assets/wallVertical.png');
+game.load.image('wallH', 'assets/wallHorizontal.png');
 
 },
 create: function() {
@@ -32,11 +34,37 @@ this.player.body.gravity.y = 500;
 // Add arrow key functionality to the game
 this.cursor = game.input.keyboard.createCursorKeys();
 
+// Create our wall group with Arcade physics
+this.walls = game.add.group();
+this.walls.enableBody = true;
+// Create the 10 walls
+game.add.sprite(0, 0, 'wallV', 0, this.walls); // Left
+game.add.sprite(480, 0, 'wallV', 0, this.walls); // Right
+game.add.sprite(0, 0, 'wallH', 0, this.walls); // Top left
+game.add.sprite(300, 0, 'wallH', 0, this.walls); // Top right
+game.add.sprite(0, 320, 'wallH', 0, this.walls); // Bottom left
+game.add.sprite(300, 320, 'wallH', 0, this.walls); // Bottom right
+game.add.sprite(-100, 160, 'wallH', 0, this.walls); // Middle left
+game.add.sprite(400, 160, 'wallH', 0, this.walls); // Middle right
+var middleTop = game.add.sprite(100, 80, 'wallH', 0, this.walls);
+middleTop.scale.setTo(1.5, 1);
+var middleBottom = game.add.sprite(100, 240, 'wallH', 0, this.walls);
+middleBottom.scale.setTo(1.5, 1);
+// Set all the walls to be immovable
+this.walls.setAll('body.immovable', true);
+
+this.createWorld();
 },
 update: function() {
 // This function is called 60 times per second
 // It contains the game's logic
+// Tell Phaser that the player and the walls should collide
+game.physics.arcade.collide(this.player, this.walls);
 this.movePlayer();
+
+if (!this.player.inWorld) {
+this.playerDie();
+}
 },
 // And here we will later add some of our own functions
 
@@ -57,7 +85,9 @@ movePlayer: function() {
 		}      
 	},
 
-
+playerDie: function() {
+game.state.start('main');
+},
 
 
 
